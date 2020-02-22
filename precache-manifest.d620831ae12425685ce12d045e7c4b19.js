c0da5cef45e68e3ce27f8d8a5382fc705b49ff63
Kislay Singh <43761382+SinghKislay@users.noreply.github.com>
2020-02-22 18:36:16 +0530

    Updates

---
+++ precache-manifest.d620831ae12425685ce12d045e7c4b19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087154bda7b2ffd6925633a57cc8b7a4",
+    "url": "/GSOC-Backend/index.html"
+  },
+  {
+    "revision": "0af765c147534ec1b6a9",
+    "url": "/GSOC-Backend/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c0915cb8fbdd15424771",
+    "url": "/GSOC-Backend/static/js/2.d61376a9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/GSOC-Backend/static/js/2.d61376a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0af765c147534ec1b6a9",
+    "url": "/GSOC-Backend/static/js/main.9e8729b7.chunk.js"
+  },
+  {
+    "revision": "53ae2694555f9ade10c7",
+    "url": "/GSOC-Backend/static/js/runtime-main.722ef095.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GSOC-Backend/static/media/logo.5d5d9eef.svg"
+  }
+]);
