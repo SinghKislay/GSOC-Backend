794d21f22644d8454e111ece4b7ae29aaf559dcc
Kislay Singh <43761382+SinghKislay@users.noreply.github.com>
2020-02-24 13:31:38 +0530

    gui changes

--- gui/src/App.js
+++ gui/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import AppBarComp from './components/AppBar'
-import AppSurface from './containers/AppSurface'
+
 
 function App() {
   return (
     <div className="App">
       <AppBarComp/>
-      
-      
-      
     </div>
   );
 }
