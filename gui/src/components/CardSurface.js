d91e06b597b38774b6a50e29d03e841d20d396bc
Kislay Singh <43761382+SinghKislay@users.noreply.github.com>
2020-02-24 13:28:29 +0530

    gui changes

--- gui/src/components/CardSurface.js
+++ gui/src/components/CardSurface.js
@@ -32,11 +32,10 @@ export default function CardSurface() {
         />
         <CardContent>
           <Typography color="inherit" gutterBottom variant="h5" component="h2">
-            Lizard
+            Disease
           </Typography>
           <Typography variant="body2" color="inherit" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop.
           </Typography>
         </CardContent>
       </CardActionArea>
