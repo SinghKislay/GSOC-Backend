06c5427d219e143bf009ab866ee8df66ff720f44
Kislay Singh <43761382+SinghKislay@users.noreply.github.com>
2020-02-29 23:19:36 +0530

    model improvements

--- training/inference.py
+++ training/inference.py
@@ -7,26 +7,9 @@ import cv2
 from create_dataset import *
 import tensorflow_hub as hub
 
-def macro_f1(y, y_hat, thresh=0.5):
-    """Compute the macro F1-score on a batch of observations (average F1 across labels)
-    
-    Args:
-        y (int32 Tensor): labels array of shape (BATCH_SIZE, N_LABELS)
-        y_hat (float32 Tensor): probability matrix from forward propagation of shape (BATCH_SIZE, N_LABELS)
-        thresh: probability value above which we predict positive
-        
-    Returns:
-        macro_f1 (scalar Tensor): value of macro F1 for the batch
-    """
-    y_pred = tf.cast(tf.greater(y_hat, thresh), tf.float32)
-    tp = tf.cast(tf.math.count_nonzero(y_pred * y, axis=0), tf.float32)
-    fp = tf.cast(tf.math.count_nonzero(y_pred * (1 - y), axis=0), tf.float32)
-    fn = tf.cast(tf.math.count_nonzero((1 - y_pred) * y, axis=0), tf.float32)
-    f1 = 2*tp / (2*tp + fn + fp + 1e-16)
-    macro_f1 = tf.reduce_mean(f1)
-    return macro_f1
 
-new_model = tf.keras.models.load_model('mymodel.h5', custom_objects={'KerasLayer':hub.KerasLayer, 'macro_f1':macro_f1})
+
+new_model = tf.keras.models.load_model('mymodel.h5', custom_objects={'KerasLayer':hub.KerasLayer})
 
 
 img = cv2.imread("C:/Users/Kislay/Desktop/GSOC/data/sample/images/00000013_005.png")
--- training/train.py
+++ training/train.py
@@ -7,7 +7,7 @@ import cv2
 from create_dataset import *
 import tensorflow_hub as hub
 from datetime import datetime
-import tensorflowjs as tfjs
+
 
 dataset = create_dataset()
 
@@ -18,43 +18,20 @@ feature_extractor_layer.trainable = False
 
 model = tf.keras.Sequential([
     feature_extractor_layer,
-    tf.keras.layers.Dense(1024, activation='relu', name='hidden_layer1'),
-    tf.keras.layers.Dense(512, activation='relu', name='hidden_layer2'),
-    tf.keras.layers.Dense(12, activation='sigmoid', name='predictions')
+    tf.keras.layers.Dense(12, activation='softmax', name='predictions')
 ])
 
-def macro_f1(y, y_hat, thresh=0.5):
-    """Compute the macro F1-score on a batch of observations (average F1 across labels)
-    
-    Args:
-        y (int32 Tensor): labels array of shape (BATCH_SIZE, N_LABELS)
-        y_hat (float32 Tensor): probability matrix from forward propagation of shape (BATCH_SIZE, N_LABELS)
-        thresh: probability value above which we predict positive
-        
-    Returns:
-        macro_f1 (scalar Tensor): value of macro F1 for the batch
-    """
-    y_pred = tf.cast(tf.greater(y_hat, thresh), tf.float32)
-    tp = tf.cast(tf.math.count_nonzero(y_pred * y, axis=0), tf.float32)
-    fp = tf.cast(tf.math.count_nonzero(y_pred * (1 - y), axis=0), tf.float32)
-    fn = tf.cast(tf.math.count_nonzero((1 - y_pred) * y, axis=0), tf.float32)
-    f1 = 2*tp / (2*tp + fn + fp + 1e-16)
-    macro_f1 = tf.reduce_mean(f1)
-    return macro_f1
 
-LR = 1e-5 # Keep it small when transfer learning
-EPOCHS = 100
+
+LR = 1e-5
+EPOCHS = 30
 
 model.compile(
   optimizer=tf.keras.optimizers.Adam(learning_rate=LR),
-  loss='binary_crossentropy',
-  metrics=[macro_f1])
+  loss='categorical_crossentropy',
+  metrics = ['accuracy']
+  )
 
 history = model.fit(dataset, epochs=EPOCHS)
 model.summary()
-
 model.save("mymodel.h5")
-
-#tfjs.converters.save_keras_model(model, 'C:/Users/Kislay/Desktop/GSOC/json')
-#tf.keras.experimental.export_saved_model(model, export_path)
-#print("Model with macro soft-f1 was exported in this path: '{}'".format(export_path))
