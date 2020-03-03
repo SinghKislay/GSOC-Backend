266b7244b58a3ab9eafb59dbb59cc2995a03ab31
Kislay Singh <43761382+SinghKislay@users.noreply.github.com>
2020-03-03 23:36:32 +0530

    Inference improvements

--- training/inference.py
+++ training/inference.py
@@ -11,9 +11,12 @@ import tensorflow_hub as hub
 
 new_model = tf.keras.models.load_model('mymodel.h5', custom_objects={'KerasLayer':hub.KerasLayer})
 
-
+new_model.summary()
 img = cv2.imread("C:/Users/Kislay/Desktop/GSOC/data/sample/images/00000013_005.png")
 img_resized = cv2.resize(img, (224, 224))
-img_resized = img_resized.reshape(1, 224, 224, 3).astype(float)
-predictions = new_model.predict(img_resized)
+gray_image_3ch = np.stack(img_resized, axis=0)/255
+gray_image_3ch = np.array([gray_image_3ch])
+#cv2.imshow('',gray_image_3ch)
+#cv2.waitKey(0)
+predictions = new_model.predict(gray_image_3ch)
 print(predictions)
